8c0caf40afbd4f92e91f9c162f42a44dc22a4674
Adam <aesawyer@live.com>
2018-06-22 22:59:18 -0600

    Initial Commit

---
+++ awsS3BucketRead.py
@@ -0,0 +1,3 @@
+# Adam Sawyer. 2018. Python-Lambda function
+# This script reads the content of an AWS S3 bucket and prints the results as a formatted .txt to be imported into an .html file
+#TODO Write code
