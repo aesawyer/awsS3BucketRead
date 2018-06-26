0330356184f407b54e3c9f35c06a5efc23cc1c64
Adam <aesawyer@live.com>
2018-06-25 20:58:00 -0600

    Dictionary -> s3 (first test)

--- awsS3BucketRead.py
+++ awsS3BucketRead.py
@@ -2,12 +2,16 @@
 # Alt-Shift-T : Show Todo Sidebar
 
 import boto3
-import os
+import os.path
+import json
 
 def lambda_handler(event, context):
     client = boto3.client('s3')
     s3 = boto3.resource('s3')
     bucketBack = s3.Bucket('MOVIE_BUCKET_NAME')
     bucketTar = s3.Bucket('TARGET_BUCKET_NAME')
-    obj = client.get_object(Bucket=bucketBack,Key='*') # TODO: resolve client.get_object return issues
-    return obj
+
+    for obj in bucketBack.objects.all():
+        files.append(obj.key, obj.metadata)
+    movieList = s3.Object(bucketTar, 'movieList.txt').put(Body=json.dumps(files))
+    return "Success"
