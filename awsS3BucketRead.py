314aac838bec1b3af2b26beb6d8aa592dd206d96
Adam <aesawyer@live.com>
2018-06-26 20:46:59 -0600

    Removed redundant comments

--- awsS3BucketRead.py
+++ awsS3BucketRead.py
@@ -1,5 +1,4 @@
 # Adam Sawyer. 2018. Python-Lambda function
-# Alt-Shift-T : Show Todo Sidebar
 
 import boto3
 import os
@@ -8,12 +7,12 @@ import json
 def lambda_handler(event, context):
     files = []
     s3 = boto3.resource('s3')
-    bucketBack = s3.Bucket(os.environ['MOVIE_BUCKET_NAME']) #utilize os.environ to pass names into EnvironmentVariables
+    bucketBack = s3.Bucket(os.environ['MOVIE_BUCKET_NAME'])
     bucketTar = s3.Bucket(os.environ['TARGET_BUCKET_NAME'])
 
     for obj in bucketBack.objects.all():
         files.append(obj.key)
     formatString = ' | '.join(files)
-    
+
     print(formatString)
     return formatString
