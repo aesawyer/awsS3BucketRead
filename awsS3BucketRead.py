69f51ea4dcf9ed297c96b69e804f79ebad5bb1fb
Adam <aesawyer@live.com>
2018-06-28 15:35:47 -0600

    Added .txt export functionality

--- awsS3BucketRead.py
+++ awsS3BucketRead.py
@@ -5,14 +5,21 @@ import os
 import json
 
 def lambda_handler(event, context):
-    files = []
     s3 = boto3.resource('s3')
     bucketBack = s3.Bucket(os.environ['MOVIE_BUCKET_NAME'])
     bucketTar = s3.Bucket(os.environ['TARGET_BUCKET_NAME'])
+    name = 'movieList.txt'
+    data = []
 
     for obj in bucketBack.objects.all():
-        files.append(obj.key)
-    formatString = ' | '.join(files)
+        data.append(obj.key)
+    formatString = ' || '.join(data)
+
+    bucketTar.put_object(
+        ACL='public-read',
+        Key=name,
+        Body=formatString
+    )
 
     print(formatString)
-    return formatString
+    return "Uploaded to " + str(bucketTar)
